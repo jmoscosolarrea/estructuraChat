4957884cdc37800a4a3cba496258a5a700917580
Jorge <jmoscosolarrea@gmail.com>
2019-12-28 12:17:30 -0500

    Segundo commit

---
+++ EstructuraChat_C6U4L3/server/Chat/index.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var Router = express.Router()
+
+Router.get('/users', function(req, res){
+
+})
+
+Router.get('/messages', function(req, res){
+
+})
+
+Router.post('/users', function(req, res){
+
+})
+
+Router.post('/messages', function(req, res){
+
+})
+
+module.exports = Router
